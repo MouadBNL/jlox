cb4f52f35bb714adca9199c467ee00bd9e057d8b
MouadBNL <mouadbnl.2k01@gmail.com>
2024-11-16 17:05:23 +0100

    feat: added for loops with desugared technique

--- src/main/java/org/example/Lox/Parser.java
+++ src/main/java/org/example/Lox/Parser.java
@@ -1,6 +1,7 @@
 package org.example.Lox;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -49,11 +50,57 @@ public class Parser {
     }
 
     private Stmt statement() {
+        if(match(TokenType.FOR)) return forStatement();
         if(match(TokenType.WHILE)) return whileStatement();
         if(match(TokenType.IF)) return ifStatement();
         if(match(TokenType.PRINT)) return printStatement();
         if(match(TokenType.LEFT_BRACE)) return new Stmt.Block(block());
         return expressionStatement();
+    }
+
+    private Stmt forStatement() {
+        consume(TokenType.LEFT_PAREN, "Expected '(' after for");
+        Stmt initializer;
+        if(match(TokenType.SEMICOLON)){
+            initializer = null;
+        } else if(match(TokenType.VAR)) {
+            initializer = varDeclaration();
+        } else {
+            initializer = expressionStatement();
+        }
+
+        Expr condition = null;
+        if(!check(TokenType.SEMICOLON)) {
+            condition = expression();
+        }
+        consume(TokenType.SEMICOLON, "Expect ';' after loop condition.");
+
+        Expr increment = expression();
+
+        consume(TokenType.RIGHT_PAREN, "Expected ')' after for clauses.");
+
+        Stmt body = statement();
+        if(increment != null) {
+            body = new Stmt.Block(
+                Arrays.asList(
+                    body,
+                    new Stmt.Expression(increment)
+                )
+            );
+        }
+
+        if(condition == null) condition = new Expr.Literal(true);
+        body = new Stmt.While(condition, body);
+        if(initializer != null) {
+            body = new Stmt.Block(
+                Arrays.asList(
+                    initializer,
+                    body
+                )
+            );
+        }
+        return body;
+
     }
 
     private Stmt whileStatement() {
